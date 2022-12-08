ac07fcce93c0bfbab439a05ac113b2c0b8aa350a
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2022-12-07 22:13:34 -0300

    Filter

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -12,22 +12,13 @@ import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 
-@WebServlet("/entrada")
+@WebServlet(urlPatterns = "/entrada")
 public class UnicaEntradaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String paramAcao = request.getParameter("acao");
-		
-		HttpSession sessao = request.getSession();
-		boolean usuarioNaoEstaLogado = (sessao.getAttribute("usuarioLogado") == null);
-		boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
-		
-		if(ehUmaAcaoProtegida && usuarioNaoEstaLogado) {
-			response.sendRedirect("entrada?acao=LoginForm");
-			return;
-		}
 		
 		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
 		
